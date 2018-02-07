f839709f0da31bb1b0fce51f4f958dbefeb4bc02
Roger <roger.santos.main@gmail.com>
2018-02-07 12:29:03 -0600

    Added Server Publisher example

--- src/main.cpp
+++ src/main.cpp
@@ -159,11 +159,63 @@ bool RunNonWaitingSubscriber()
         }
 
         printf("not done. Sleeping for 1 second\n");
-        usleep(1*1000*1000);
+        usleep(1 * 1000 * 1000);
     }
 
     printf("Stopping subscriber.\n");
     socket.close();
+    return true;
+}
+
+bool RunServerPublisher()
+{
+    zmq::context_t context(1);
+    zmq::socket_t server_socket(context, ZMQ_REP);
+    zmq::socket_t publisher_socket(context, ZMQ_PUB);
+    std::string server_uri = "tcp://*:9999";
+    std::string publisher_uri = "tcp://*:9998";
+    server_socket.bind(server_uri.c_str());
+    publisher_socket.bind(publisher_uri.c_str());
+    printf("\nServer Publisher\n\tServer URL: tcp://localhost:9999\n\tPublisher URL: tcp://localhost:9998\n\n");
+
+    std::string value_to_publish("None - Send the Value using a Client");
+    while (value_to_publish.compare("exit"))
+    {
+        printf("\tVerifying for Client request ...");
+        fflush(stdout);
+
+        zmq::message_t request_message;
+        if (server_socket.recv(&request_message, ZMQ_DONTWAIT))
+        {
+            printf("done.\n");
+            printf("\t\tMessage: Size: %d ; Value: %s\n", static_cast<int>(request_message.size()), static_cast<const char*>(request_message.data()));
+            value_to_publish.assign(static_cast<const char*>(request_message.data()), request_message.size() - 1);
+
+            printf("\tSending reply:\n");
+
+            std::string server_reply("Set new value to publish: ");
+            server_reply += value_to_publish;
+
+            zmq::message_t reply_message((void*)server_reply.c_str(), server_reply.size() + 1);
+            printf("\t\tMessage: Size: %d ; Value: %s\n", static_cast<int>(reply_message.size()), static_cast<const char*>(reply_message.data()));
+            server_socket.send(reply_message);
+        }
+        else
+        {
+            printf("done. No new message. Sleeping for 1 second.\n");
+            usleep(1 * 1000 * 1000);
+        }
+
+        printf("\tPublishing Value: %s\n", value_to_publish.c_str());
+
+        zmq::message_t publisher_message((void*)value_to_publish.c_str(), value_to_publish.size() + 1);
+        printf("\t\tMessage: Size: %d ; Value: %s\n", static_cast<int>(publisher_message.size()), static_cast<const char*>(publisher_message.data()));
+        publisher_socket.send(publisher_message);
+    }
+
+    printf("Stopping Server Publisher.\n");
+    server_socket.close();
+    publisher_socket.close();
     return true;
 }
 
@@ -189,11 +241,16 @@ int main(int argc, char const* argv[])
         RunSubscriber();
     else if (!argument.compare("non_waiting_subscriber"))
         RunNonWaitingSubscriber();
+    else if (!argument.compare("server_publisher"))
+        RunServerPublisher();
     else
         std::cout << "Invalid Command (" << argc << "):\n\t" << argument << "\nOptions:\n"
+                  << "\tserver\n"
+                  << "\tclient\n"
                   << "\tpublisher\n"
                   << "\tsubscriber\n"
                   << "\tnon_waiting_subscriber\n"
+                  << "\tserver_publisher\n"
                   << "\n";
 
     return 0;
